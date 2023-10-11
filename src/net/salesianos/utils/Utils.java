b86330014c0bf0a4ea0f980d3de467c749f31134
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-10-11 08:42:07 +0100

    feat: Creada la clase Utils

---
+++ src/net/salesianos/utils/Utils.java
@@ -0,0 +1,40 @@
+package net.salesianos.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Utils {
+    public static ArrayList<String> getAllTextLineCounter(String fileRoute) {
+        ArrayList<String> TextLineCounter = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+
+            String currentLine = bufferedReader.readLine();
+
+            while (currentLine != null) {
+                TextLineCounter.add(currentLine);
+                currentLine = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return TextLineCounter;
+    }
+
+    public static Integer getTotalCharsFrom(String fileRoute) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+            String firsLine = bufferedReader.readLine();
+            bufferedReader.close();
+            return Integer.parseInt(firsLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
