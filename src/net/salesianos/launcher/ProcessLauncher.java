69dd4f6c5b40d72c581dccdd8d2a919dd6ca6edd
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-10-11 08:24:51 +0100

    feat: Creada la clase ProcessLauncher con un método estático

---
+++ src/net/salesianos/launcher/ProcessLauncher.java
@@ -0,0 +1,25 @@
+package net.salesianos.launcher;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ProcessLauncher {
+    private final static String OUTPUT_ROUTE = "src/net/salesianos/outputs/";
+
+    public static Process initVowelCounter(String text, String vowel, String fileName) {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "java", "src/net/salesianos/processes/CharCounter.java", text, vowel
+        );
+
+        Process javaProcess = null;
+
+        try {
+            File outputFile = new File(OUTPUT_ROUTE + fileName);
+            processBuilder.redirectOutput(outputFile);
+            javaProcess = processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return javaProcess;
+    }
+}
